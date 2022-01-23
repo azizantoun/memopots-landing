2bab41a0f071cb95807e3e4029a460209471a0c2
azizantoun <aziz@miro.com>
2022-01-23 13:30:08 +0100

    test pdf

--- next.config.js
+++ next.config.js
@@ -5,4 +5,16 @@ const nextConfiguration = {
   target: 'serverless', //will output independent pages that don't require a monolithic server. It's only compatible with next start or Serverless deployment platforms (like ZEIT Now) — you cannot use the custom server API.
 };
 
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/letters/03012022',
+        destination: '/',
+      },
+    ]
+  },
+}
+
+
 module.exports = withPlugins([optimizedImages], nextConfiguration);
---
+++ src/pages/letters/[letter_id].js
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+import dynamic from 'next/dynamic';
+const FileViewer = dynamic(() => import('react-file-viewer'), {
+    ssr: false
+});
+
+function Letter() {
+    return (
+        <div>
+            <FileViewer fileType="pdf" filePath="/03012022.pdf" />
+        </div>
+    )
+}
+
+export default Letter
